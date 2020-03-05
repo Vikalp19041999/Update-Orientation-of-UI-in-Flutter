641ed9b7460c1a791997fded180a003ba646ddf7
Vikalp Chakravorty <Vicks548@gmail.com>
2020-03-05 17:05:51 +0530

    Updating the orientation of UI from portrait to landscape in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: OrientationList(),
+    );
+  }
+}
+
+class OrientationList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Update Orientation on UI'),
+        centerTitle: true,
+        backgroundColor: Colors.black,),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.count(
+            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+            children: List.generate(100, (index) {
+              return Center(
+                child: Text(
+                  'Item $index',
+                  style: Theme.of(context).textTheme.headline,
+                ),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
